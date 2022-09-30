72819420f44dfa80df82f88b8ebc0b83f08d8a0d
Andrii Korshenko <andriikorshenko@gmail.com>
2022-10-01 01:27:04 +0300

    Added Bootstrap style and some elements

---
+++ ToDoList/ToDoListWeb/Controllers/AssignmentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoListWeb.Data;
+using ToDoListWeb.Models;
+
+namespace ToDoListWeb.Controllers
+{
+    public class AssignmentController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AssignmentController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Assignment> objAssignmentList = _db.Assignments;
+            return View(objAssignmentList);
+        }
+    }
+}
