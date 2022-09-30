0736cbd2394093a59761f0c4f4fc33c6308f8fb7
Andrii Korshenko <andriikorshenko@gmail.com>
2022-09-30 23:39:18 +0300

    Added a DataBase context and migrated to a local MSSQL Server

---
+++ ToDoList/ToDoListWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoListWeb.Models;
+
+namespace ToDoListWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<Assignment> Assignments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Assignment>()
+                .HasKey(x => x.Id);
+
+            modelBuilder.Entity<Assignment>()
+                .Property(x => x.Text).IsRequired();
+        }
+    }
+}
--- ToDoList/ToDoListWeb/Models/Assignment.cs
+++ ToDoList/ToDoListWeb/Models/Assignment.cs
@@ -2,5 +2,9 @@
 {
     public class Assignment
     {
+        public uint Id { get; set; }
+        public string? Text { get; set; }
+        public uint DisplayOrder { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
 }
