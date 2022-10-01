9f446ee43ff81faea6ef3459df45b206ed989349
Andrii Korshenko <andriikorshenko@gmail.com>
2022-10-01 23:14:23 +0300

    Deployed to Azure. Added some minor changes

---
+++ ToDoList/ToDoListWeb/obj/Release/net6.0/win-x86/PubTmp/EFSQLScripts/ToDoListWeb.ApplicationDbContext.sql
@@ -0,0 +1,35 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20221001140005_AddNew')
+BEGIN
+    CREATE TABLE [Assignments] (
+        [Id] int NOT NULL IDENTITY,
+        [Name] nvarchar(max) NOT NULL,
+        [Text] nvarchar(max) NOT NULL,
+        [CreatedAt] datetime2 NOT NULL,
+        CONSTRAINT [PK_Assignments] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20221001140005_AddNew')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20221001140005_AddNew', N'6.0.9');
+END;
+GO
+
+COMMIT;
+GO
+
