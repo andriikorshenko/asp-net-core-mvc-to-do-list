ad77fa90f6c5fe6fc9ce21528528e380a6ba66d6
Andrii Korshenko <andriikorshenko@gmail.com>
2022-10-01 10:52:35 +0300

    Added Create form

---
+++ ToDoList/ToDoListWeb/Migrations/20221001072444_AddedNameColumnAndDelitedOrderColumn.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoListWeb.Migrations
+{
+    public partial class AddedNameColumnAndDelitedOrderColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisplayOrder",
+                table: "Assignments");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Assignments",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Assignments");
+
+            migrationBuilder.AddColumn<long>(
+                name: "DisplayOrder",
+                table: "Assignments",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+    }
+}
